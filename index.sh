970c16cd7c33c38acb0dbc03ab5a6a3672af23d7
feizheng <1290657123@qq.com>
2016-10-28 20:51:59 +0800

    edit cron

--- index.sh
+++ index.sh
@@ -9,7 +9,7 @@ alias cron-stop='service crond stop';
 alias cron-restart='service crond restart';
 alias cron-reload='service crond reload';
 alias cron-status='service crond status';
-alias e-cron='';
+alias e-cron='crontab -e';
 
 
 unset ROOT_PATH;
