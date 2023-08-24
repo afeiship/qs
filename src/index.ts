640a8832aa397cc88d778205b82059b461dd4ca6
afeiship <1290657123@qq.com>
2023-08-24 22:49:28 +0800

    feat: add str opts

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,54 @@ const MSG = {
   NOT_SUPPORT: 'Please use url-search-params-polyfill',
 };
 
+interface IStringifyOptions {
+  /**
+   * 是否添加查询前缀
+   */
+  addQueryPrefix?: boolean;
+  /**
+   * 数组格式
+   */
+  arrayFormat?: 'bracket' | 'index' | 'none';
+  /**
+   * 是否编码
+   */
+  encode?: boolean;
+  /**
+   * 是否只编码值
+   */
+  encodeValuesOnly?: boolean;
+  /**
+   * 是否排除空值
+   */
+  ignoreEmptyString?: boolean;
+}
+
+const defaultStringifyOptions: IStringifyOptions = {
+  addQueryPrefix: false,
+  arrayFormat: 'none',
+  encode: true,
+  encodeValuesOnly: false,
+  ignoreEmptyString: false,
+};
+
 class Qs {
   constructor() {
     const supportURLSearchParams = typeof URLSearchParams !== 'undefined';
     if (!supportURLSearchParams) console.warn(MSG.NOT_SUPPORT);
   }
 
-  public stringify(inObj, inOptions?) {
+  public stringify(inObj, inOptions?: IStringifyOptions) {
+    const opts: IStringifyOptions = { ...defaultStringifyOptions, ...inOptions };
     const params = new URLSearchParams();
-    Object.keys(inObj).forEach((key) => {
-      params.append(key, inObj[key]);
-    });
-    return params.toString();
+
+    // const params = new URLSearchParams();
+    // Object.keys(inObj).forEach((key) => {
+    //   params.append(key, inObj[key]);
+    // });
+    // return params.toString();
   }
+
   public parse(inString, inOptions?) {
     const params = new URLSearchParams(inString);
     const obj = {};
