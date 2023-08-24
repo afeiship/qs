b18f580220556827b5597d3763c093bce41d5472
afeiship <1290657123@qq.com>
2023-08-24 22:42:07 +0800

    feat: basic impl

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,12 @@
 import fn from '../src';
 
 describe('api.basic', () => {
-  test('normail single value case', () => {
-    expect(fn({ rel: true })).toBe(undefined);
+  test('normail case parse/stringify', () => {
+    const obj = { a: 1, b: 2 };
+    const str = fn.stringify(obj);
+    const obj2 = fn.parse(str);
+
+    expect(str).toBe('a=1&b=2');
+    expect(obj2).toEqual({ a: '1', b: '2' });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
 declare var wx: any;
 
-const Qs = (): void => {
-  console.log('hello');
+const MSG = {
+  NOT_SUPPORT: 'Please use url-search-params-polyfill',
 };
+
+class Qs {
+  constructor() {
+    const supportURLSearchParams = typeof URLSearchParams !== 'undefined';
+    if (!supportURLSearchParams) console.warn(MSG.NOT_SUPPORT);
+  }
+
+  public stringify(inObj, inOptions?) {
+    const params = new URLSearchParams();
+    Object.keys(inObj).forEach((key) => {
+      params.append(key, inObj[key]);
+    });
+    return params.toString();
+  }
+  public parse(inString, inOptions?) {
+    const params = new URLSearchParams(inString);
+    const obj = {};
+    params.forEach((value, key) => {
+      obj[key] = value;
+    });
+    return obj;
+  }
+}
+
+const qs = new Qs();
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
-  module.exports = Qs;
+  module.exports = qs;
 }
 
-export default Qs;
+export default qs;
